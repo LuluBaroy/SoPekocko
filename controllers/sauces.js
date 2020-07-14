e30fd6f3f376bbaf02739f1127b6ce6cca4d8ae2
Lulu Baroy <lucie.baroiller@gmail.com>
2020-07-14 09:08:51 +0200

    Amélioration de la méthode PUT (suppression image précédente si nouvelle image ajoutée)

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -57,6 +57,18 @@ exports.modifyOneSauce = (req, res, next) => {
 			...JSON.parse(req.body.sauce),
 			imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
 		} : { ...req.body };
+	if(req.file){
+		Sauces.findOne({_id: req.params.id})
+			.then(sauce => {
+				const filename = sauce.imageUrl.split('/images/')[1];
+				fs.unlink(`images/${filename}`, (error) => {
+					if( error ){
+						console.log("Error : Deleting failed !");
+					}
+				})
+			})
+			.catch(error => res.status(500).json({ error }))
+	}
 	Sauces.updateOne({ _id: req.params.id }, { ...sauceModified, _id: req.params.id })
 		.then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
 		.catch(error => res.status(400).json({ error }));
