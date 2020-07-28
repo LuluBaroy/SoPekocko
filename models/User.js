e120588f34c0f8976b6bc109d09d09deee88b1e5
Lulu Baroy <lucie.baroiller@gmail.com>
2020-07-28 10:59:54 +0200

    Ajout des routes pour RGPD

--- models/User.js
+++ models/User.js
@@ -5,6 +5,14 @@ const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true }
+	/*birthdate: { type: Date, required: true },
+	 *parentEmail: { type: String, required: true }  // if user is a child
+	 *restricted: { type: Boolean, required: true },
+	 *consents: {
+	 * 	shareWithPartners: { type: Boolean, required: true },
+	 * 	contactable: { type: Boolean, required: true}
+	 * }
+	 */
 })
 
 userSchema.plugin(uniqueValidator);
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,20 @@ const router = express.Router();
 const validator = require('../middleware/validator');
 const userCtrl = require('../controllers/users');
 const bouncer = require('express-bouncer')(15000, 30000, 3);
+const auth = require('../middleware/auth');
 'use strict';
 
 router.post('/signup', validator.checkingSignup, userCtrl.signup);
 
 router.post('/login', bouncer.block, userCtrl.login);
 
+router.get('/:id', auth, userCtrl.readOne);
+
+router.put('/:id', auth, userCtrl.update);
+
+router.put('/:id/reports', auth, userCtrl.report);
+
+router.delete('/:id', auth, userCtrl.delete);
+
+router.post('/:id/exports', auth, userCtrl.export);
 module.exports = router;
